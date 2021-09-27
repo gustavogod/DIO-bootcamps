074814092ff508ad8657f10dc77641a4f56408e7
gustavogod <gustavodias.god@gmail.com>
2021-09-27 09:44:56 -0300

    Curso Trabalhando com Componentes em React concluído

---
+++ Curso React LocalizaLabs/Trabalhando com Componentes em React/react-dio/src/App.js
@@ -0,0 +1,24 @@
+import Card from './components/Card';
+import Item from './components/Item';
+
+const App = () => {
+  return (
+    <>
+      <h1>Minha primeira aplicação com React</h1>
+      <ul>        
+        <Item>
+          Item 1  
+        </Item>
+        <Item>
+          Item 2
+        </Item>
+        <Item>
+          Item 3  
+        </Item>
+      </ul>
+      <Card/>
+    </>
+  )
+}
+
+export default App;
