b937463dd059df7b859dc87d475c2d3ccefbb798
gustavogod <gustavodias.god@gmail.com>
2021-10-25 11:34:52 -0300

    folders updated

--- Curso React LocalizaLabs/Introdução ao JS ES6/design patterns/module-import.js
+++
@@ -1,7 +0,0 @@
-// continuação do module
-// usando o module criado no outro arquivo
-const {getName, setName} = require('./module.js');
-
-console.log(getName());
-console.log(setName('Gustavo'));
-console.log(getName());
